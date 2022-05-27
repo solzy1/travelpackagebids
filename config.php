69fe603f5fd955f30902c60fcd3b54da46947481
Fidelis Solomon <solomonfidelis012@gmail.com>
2022-05-27 01:49:26 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,4 @@
 	define('DBNAME', 'travelpackagebids');
 	define('DBUSER', 'root');
 	define('DBPASS', '');
-	// define('DBNAME', 'travelpa_travelpackagebids');
-	// define('DBUSER', 'travelpa_travelpackagebids');
-	// define('DBPASS', 'traveltheauctionpackagebidsfor');
 ?>
